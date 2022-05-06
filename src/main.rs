64cface4c86e9cc0e6097afb43429773264bfc2b
Agustin Carnevale <agustinc.dev@gmail.com>
2022-05-06 15:54:18 -0300

    refactor/improvements

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 mod theme;
 
-use newsapi::{get_articles, Articles};
 use std::error::Error;
-// use colour::{dark_green,yellow};
+use newsapi::{NewsAPI, Endpoint, Country, Article};
 
-fn render_articles(articles: &Articles) {
+fn render_articles(articles: &Vec<Article>) {
     let theme = theme::default();
     theme.print_text("# Top headlines\n\n");
-    for article in &articles.articles {
-        theme.print_text(&format!("`{}`", article.title));
-        theme.print_text(&format!("> *{}*", article.url));
+    for article in articles {
+        theme.print_text(&format!("`{}`", article.title()));
+        theme.print_text(&format!("> *{}*", article.url()));
         theme.print_text("---");
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let api_key = std::env::var("API_KEY")?;
 
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=";
+    let mut newsapi = NewsAPI::new(&api_key);
+    newsapi.endpoint(Endpoint::TopHeadlines).country(Country::Us); 
+    let response = newsapi.fetch_async().await?;
 
-    let url = format!("{}{}", url, api_key);
-
-    let articles = get_articles(&url)?;
-
-    render_articles(&articles);
+    render_articles(&response.articles());
     Ok(())
 }
